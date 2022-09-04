54edce993b63cd61df41f702ba89b40859a7c1a6
MohdArif112 <102711807+MohdArif112@users.noreply.github.com>
2022-09-04 12:32:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
+let count =0;
 function App() {
   function addPara(){
+    count++;a
+    if(count %2 ==1){
     const para = document.getElementById("para").innerText = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+    }
+    else{
+       const para = document.getElementById("para").innerText = ""
+    }
   }
   return (
     <div id="main">
